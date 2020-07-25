08e2110fc11f0db219fb83da931329d8001de7ca
initGoomba <anton.j.olsson@bredband.net>
2020-07-26 00:41:16 +0200

    Circle start and end points

--- app/src/main/java/com/antware/joggerlogger/MapsFragment.kt
+++ app/src/main/java/com/antware/joggerlogger/MapsFragment.kt
@@ -18,19 +18,18 @@ import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
-import com.google.android.gms.maps.model.BitmapDescriptorFactory
-import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.MarkerOptions
-import com.google.android.gms.maps.model.PolylineOptions
+import com.google.android.gms.maps.model.*
 
 @Suppress("PrivatePropertyName", "SameParameterValue")
 class MapsFragment : Fragment() {
 
+    private val CIRCLE_Z_INDEX = 1F
+    private val STATUS_CHANGED_CIRCLE_RADIUS = 10.0
+    private val POLYLINE_WIDTH = 25F
     private val STOP_EXERCISE_MARKER_COLOR = 0F
     private val INITIAL_ZOOM_LEVEL = 16.0f
     private val DEFAULT_MAP_TYPE = GoogleMap.MAP_TYPE_HYBRID
     private val START_EXERCISE_MARKER_COLOR = 171F
-    private val POLYLINE_ALPHA = 255
 
     private var map : GoogleMap? = null
     var currLocation : Location? = null
@@ -44,7 +43,6 @@ class MapsFragment : Fragment() {
     }
 
     private fun moveCameraToCurrentLoc(location: Location?) {
-
         val here = location?.latitude?.let { LatLng(it, location.longitude) }
         requireActivity().runOnUiThread {
             map?.moveCamera(CameraUpdateFactory.newLatLngZoom(here, INITIAL_ZOOM_LEVEL))
@@ -57,17 +55,11 @@ class MapsFragment : Fragment() {
     }
 
     private fun addPolyline(startPoint: LatLng?) {
-        val size = model.waypoints.size
-        val endPoint = if (size > 1) getLatLng(model.waypoints[size - 2].location) else startPoint
+        val numWaypoints = model.waypoints.size
+        val endPoint = if (numWaypoints > 1) getLatLng(model.waypoints[numWaypoints - 2].location) else startPoint
         val options = PolylineOptions().color(ContextCompat.getColor(requireActivity(), R.color.colorPrimary))
+           // .pattern(listOf(Dot())).width(POLYLINE_WIDTH)
         map?.addPolyline(options.add(startPoint, endPoint))
-    }
-
-    private fun getArgb(color: Int, alpha: Int): Int {
-        val red = Color.red(color)
-        val green = Color.green(color)
-        val blue = Color.blue(color)
-        return (alpha shl 24) or (Color.red(color) shl 16) or (Color.green(color) shl 8) or Color.blue(color)
     }
 
     @SuppressLint("MissingPermission")
@@ -112,15 +104,22 @@ class MapsFragment : Fragment() {
 
     private fun onStatusChanged(status: LogViewModel.ExerciseStatus?) {
         when (status) {
-            STARTED -> { map?.addMarker(getLatLng(currLocation)?.let { MarkerOptions().position(it).icon(
-                    BitmapDescriptorFactory.defaultMarker(START_EXERCISE_MARKER_COLOR)) })
-                model.addWaypoint(ExerciseLocation(currLocation, STARTED))
+            STARTED -> {
+                addCircle(STATUS_CHANGED_CIRCLE_RADIUS, Color.WHITE, R.color.colorPrimary, CIRCLE_Z_INDEX)
             }
-            else -> { map?.addMarker(getLatLng(currLocation)?.let { MarkerOptions().position(it).icon(
-                BitmapDescriptorFactory.defaultMarker(STOP_EXERCISE_MARKER_COLOR)) })
+            else -> {
+                addCircle(STATUS_CHANGED_CIRCLE_RADIUS, Color.WHITE, R.color.colorAccent, CIRCLE_Z_INDEX)
                 model.addWaypoint(ExerciseLocation(currLocation, status))
             }
         }
+        model.addWaypoint(ExerciseLocation(currLocation, status))
+    }
+
+    private fun addCircle(radius: Double, strokeColor: Int, fillColorId: Int, zIndex: Float) {
+        map?.addCircle(getLatLng(this.currLocation)?.let {
+            CircleOptions().center(it).radius(radius).strokeColor(strokeColor).fillColor(ContextCompat.getColor(requireActivity(),
+                fillColorId)).zIndex(zIndex)
+        })
     }
 
     private fun getLatLng(currLocation: Location?): LatLng? {
